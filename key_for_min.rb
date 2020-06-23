646b8cc136e3e2916fde5f243f7395470c81cfbb
Alex Blumenthaler <blumenthaleralex@gmail.com>
2020-06-23 18:47:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,6 @@ def key_for_min_value(name_hash)
   min_k = nil
   min_v = nil
   name_hash.each do |key, value|
-    # returns the KEY of the smallest VALUE in the hash
     if min_v == nil
       min_v = value
       min_k = key
